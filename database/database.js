2cac0c7c4e12a12f7a52225bf7238d2f3eb0843d
jaskaran <developer.jaskaran@gmail.com>
2020-09-04 17:17:17 -0400

    update api

--- database/database.js
+++ database/database.js
@@ -82,10 +82,14 @@ const loginUser = (request, response) => {
     if (error) {
       throw error
     }
-    if(results.rows.length)
-    response.status(200).json({status: 200, message: 'Welcome',data : results.rows})
-    else
-    response.status(400).json({status: 400, message: "User doesn't exist"})
+    if(results.rows.length){
+      response.status(200).json({status: 200, message: 'Welcome',data : results.rows})
+    }
+   
+    else{
+      response.status(400).json({status: 400, message: "User doesn't exist"})
+    }
+   
   })
 }
 
@@ -193,22 +197,18 @@ const updateUser = (request, response) => {
       throw error
     }
     else{
-      pool.query('SELECT id, first_name,last_name,email_id,categories FROM users WHERE email_id=$1 AND password=$2',[request.body.email,request.body.password], (error, resultsNew) => {
+      pool.query('SELECT id, first_name,last_name,email_id,categories FROM users WHERE id='+request.body.id, (error, resultsNew) => {
         if (error) {
           throw error
         }
         if(resultsNew.rows.length){
           response.status(200).json({status: 200, message: 'Profile updated',data : resultsNew.rows})
           response.end()
-        }
-        
+        }        
         else{
           response.status(400).json({status: 400, message: ""})
-        }
-        
+        }        
       })
-
-
       response.status(200).json({status: 200, message: 'Profile updated'});
       response.end()
     }
